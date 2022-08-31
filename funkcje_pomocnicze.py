041b1b715cf916e3e1e1360615934e5901f98710
Hamilkar247 <adonnibal96@gmail.com>
2022-08-31 16:44:47 +0200

    [WO-14] dodanie plikow z libka funkcje_pomocnicze
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ funkcje_pomocnicze.py
@@ -0,0 +1,110 @@
+# - *- coding: utf-8 - *-
+
+from inspect import trace
+import sys
+import os
+from datetime import datetime, timedelta
+import traceback
+from dotenv import load_dotenv
+import psutil
+
+class ExceptionEnvProjektu(Exception):
+    pass
+
+class ExceptionNotExistFolder(Exception):
+    pass
+
+class ExceptionWindows(Exception):
+    pass
+
+class ExceptionRepository(Exception):
+    pass
+
+class ExceptionVirtualenv(Exception):
+    pass
+
+class ExceptionExistInstanceOfProcess(Exception):
+    pass
+
+#############
+
+class FunkcjePomocnicze():
+
+    def __init__(self, nazwa_programu, czy_drukuj=True):
+        self.nazwa_programu=nazwa_programu
+        self.czy_drukuj=czy_drukuj
+
+    def data_i_godzina(self):
+        now = datetime.now()
+        current_time = now.strftime("%d/%m/%y %H:%M:%S")
+        return current_time
+    
+    def set_drukuj(self, czy_drukuj):
+        self.czy_drukuj=czy_drukuj
+
+    def drukuj(self, obiekt_do_wydruku):
+        try:
+            if self.czy_drukuj==True:
+                print(self.data_i_godzina()+f" pid:{os.getpid()}  "+self.nazwa_programu+" "+str(obiekt_do_wydruku))
+        except Exception as e:
+            print(e)
+            print(traceback.print_exc())
+    
+    def przerwij_i_wyswietl_czas(self):
+        czas_teraz = datetime.now()
+        current_time = czas_teraz.strftime("%H:%M:%S")
+        print("Current Time =", current_time)
+        sys.exit()
+    
+    #########################
+    
+    def file_istnienie(self, path_to_file, komunikat):
+        if os.path.exists(path_to_file) == False:
+            self.drukuj(f"{komunikat}")
+            raise ExceptionEnvProjektu
+        return True
+    
+    def folder_istnienie(self, path_to_folder, komunikat):
+        if os.path.isdir(path_to_folder) == False:
+            self.drukuj(f"{komunikat}")
+            raise ExceptionEnvProjektu
+        return True
+    
+    def folder_istnienie_2(self, path_to_folder, komunikat):
+        if os.path.isdir(path_to_folder) == False:
+            self.drukuj(f"{komunikat}")
+            raise ExceptionNotExistFolder
+        return path_to_folder
+    
+    def zmienna_env_file(self, tag_in_env, komunikat):
+        path_to_file=os.getenv(tag_in_env)
+        if os.path.exists(path_to_file) == False:
+            self.drukuj(f"{komunikat}, tag:{tag_in_env}, path:{path_to_file}")#sprawdz czy plik .env istnieje")
+            raise ExceptionEnvProjektu
+        return path_to_file
+    
+    def zmienna_env_folder(self, tag_in_env, komunikat):
+        path_to_folder=os.getenv(tag_in_env)
+        if os.path.isdir(path_to_folder) == False:
+            self.drukuj(f"{komunikat}, tag:{tag_in_env}, path:{path_to_folder}")#sprawdz czy plik .env istnieje")
+            raise ExceptionEnvProjektu
+        return path_to_folder
+    
+    def usun_flare(self, folder_do_sprawdzenia, flara_do_sprawdzenia):
+        if os.path.isdir(folder_do_sprawdzenia):
+            if os.path.exists(flara_do_sprawdzenia):
+                os.remove(flara_do_sprawdzenia)
+                self.drukuj("usuwam flare")
+    
+    def stworz_flare_z_pid(self, flara_path):
+        flara_file=open(flara_path, "w")
+        flara_file.write(f"{str(os.getpid())}")
+        flara_file.close()
+
+    def sprawdz_czy_program_o_tym_pid_dziala(self, pid):
+        if psutil.pid_exists(pid):
+            self.drukuj("a process with pid %d exists" % pid)
+            return True
+        else:
+            self.drukuj("a process with pid %d does not exist" % pid)
+            return False
--- python_reboot.py
+++ python_reboot.py
@@ -2,7 +2,19 @@
 
 import os
 import sys 
+from funkcje_pomocnicze import ExceptionExistInstanceOfProcess, ExceptionVirtualenv, ExceptionRepository, FunkcjePomocnicze, ExceptionWindows, ExceptionNotExistFolder, ExceptionEnvProjektu
 
-print("proba reboota")
-os.system('/sbin/reboot')
+def nazwa_programu():
+    return "pliki_z_logami_programu_i_ich_utylizacja.py"
 
+def funkcje_pomocnicze_inicjalizacja():
+    fp=FunkcjePomocnicze(nazwa_programu())
+    return fp
+
+def main():
+    fp=funkcje_pomocnicze_inicjalizacja()
+    fp.drukuj("proba reboota")
+    os.system('/sbin/reboot')
+
+if __name__ == "__main__":
+    main()
