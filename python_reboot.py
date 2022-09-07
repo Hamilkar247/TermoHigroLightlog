4a53b2d5a21e178f6f4ec2288934676deefbd0a1
Hamilkar247 <adonnibal96@gmail.com>
2022-09-07 11:24:15 +0200

    [WO-27] dodanie codding utf-8
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- python_reboot.py
+++ python_reboot.py
@@ -1,3 +1,5 @@
+# - *- coding: utf-8 - *-
+
 import os
 import sys 
 from funkcje_pomocnicze import ExceptionExistInstanceOfProcess, ExceptionVirtualenv, ExceptionRepository, FunkcjePomocnicze, ExceptionWindows, ExceptionNotExistFolder, ExceptionEnvProjektu
