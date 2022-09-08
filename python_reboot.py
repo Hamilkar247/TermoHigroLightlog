b4c273364941c5970894a3455671f2d1fdbad4f0
Hamilkar247 <adonnibal96@gmail.com>
2022-09-08 10:21:08 +0200

    [WO-30] zapomnialem usunac reboota
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- python_reboot.py
+++ python_reboot.py
@@ -22,7 +22,7 @@ def main():
     fp=funkcje_pomocnicze_inicjalizacja()
     if sprawdz_lsusb() > 0:
         fp.drukuj("proba reboota")
-        #os.system('/sbin/reboot') 
+        os.system('/sbin/reboot') 
         fp.drukuj("jesli reboot sie odbyl poprawnie nie zobaczysz tej wiadomosci")       
     else:
         fp.drukuj("nie ma potrzeby reboota")
